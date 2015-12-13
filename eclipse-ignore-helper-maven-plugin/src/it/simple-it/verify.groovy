efd09d5f8be2d3594398a0c2631b808f4d52b001
Ronny Polley <ronnypolley@googlemail.com>
2015-12-13 14:20:38 +0100

    add integration test

--- eclipse-ignore-helper-maven-plugin/src/it/simple-it/verify.groovy
+++ eclipse-ignore-helper-maven-plugin/src/it/simple-it/verify.groovy
@@ -1,3 +1,15 @@
-File touchFile = new File( basedir, "target/touch.txt" );
+import javax.xml.xpath.*
+import javax.xml.parsers.DocumentBuilderFactory
 
-assert touchFile.isFile()
+def classpath = new File(basedir, ".classpath").getText()
+
+assert processXml(classpath, "//classpathentry[@path='src/test/java']/attributes/attribute[@name='ignore_optional_problems']") != null
+
+
+def processXml( String xml, String xpathQuery ) {
+	def xpath = XPathFactory.newInstance().newXPath()
+	def builder     = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+	def inputStream = new ByteArrayInputStream( xml.bytes )
+	def records     = builder.parse(inputStream).documentElement
+	xpath.evaluate( xpathQuery, records )
+}
